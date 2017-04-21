ad063bbe6432e01e06b9a7fa4cf3736a7357eb8e
chance304 <chancer.304@gmail.com>
2017-04-21 22:52:19 +0530

    Heroku Deploy1

---
+++ greetme.php
@@ -0,0 +1,4 @@
+<?php
+	$name=$_POST['naam'];
+	echo '<h1>Hello Mr. '.$name.'</h1>';
+?>
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo 'This is all i wanted';
+<!doctype HTML>
+<html>
+	<head>
+		<Title> Greeting By Cloud</Title>
+	</head>
+	<body>
+		<h1>Greeting From Heroku Cloud to F1 SLOT MCA</h1>
+		<h2>Please Enter your name</h2>
+		<form action="greetme.php " method="post">
+			<input type="text" name="naam" /><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
